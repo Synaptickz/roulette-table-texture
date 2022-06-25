f40b1e959136ba79ac30b30d8c0d9176512a68b6
Synaptickz <dbsoare@gmail.com>
2022-06-25 19:07:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pwppu2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/pwppu2)
---
+++ index.js
@@ -0,0 +1,23 @@
+import './style.scss';
+import { saveAsPng, saveAsJpeg } from 'save-html-as-image';
+import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
+
+const b = document.getElementById('save');
+b.onclick = () => {
+  const node = document.getElementById('gen');
+  toPng(node).then((dataUrl) => {
+    // const img = new Image();
+    // img.src = dataUrl;
+    // document.body.append(img);
+
+    const a = document.createElement('a');
+    a.setAttribute('download', 'table-texture.png');
+    a.setAttribute('href', dataUrl);
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  });
+};
+
+console.log('CSS Grid created with https://grid.layoutit.com');
+console.log('Powered by https://vitejs.dev');
