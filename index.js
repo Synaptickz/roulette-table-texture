f012645322a07bd5ae5a8c6f2ec167dc5f05e5af
Synaptickz <dbsoare@gmail.com>
2022-06-25 19:11:51 +0300

    1 file modified

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ b.onclick = () => {
     document.body.removeChild(a);
   });
 };
+//
 
 console.log('CSS Grid created with https://grid.layoutit.com');
 console.log('Powered by https://vitejs.dev');
